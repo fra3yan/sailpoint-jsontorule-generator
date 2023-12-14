0d9497429faea6d0c84872e57f577d3c892f7f37
Anggi Fratriyan Pratama <fra3yan@gmail.com>
2023-12-14 14:30:46 +0700

    update code

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+$json_file = file_get_contents('C:\temp\branch.json');  // where you put json file
+$json_data = json_decode($json_file);
+$error = json_last_error();
+
+//if you cannot read json file or error file
+//var_dump($json_data, $error === JSON_ERROR_UTF8);
+
+
+
+
+
+//config
+$namefile = "role.java"; //generated file name
+
+
+//header download
+header("Content-Disposition: attachment; filename=\"" . $namefile . "\"");
+header("Content-Type: application/force-download");
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header("Content-Type: text/plain");
+
+
+
+
+$getAttribute = 'location_code';
+echo '
+import sailpoint.object.Identity;
+import sailpoint.object.Link;
+import sailpoint.object.Application;
+
+String val = "";
+String getAttribute = identity.getAttribute("'.$getAttribute.'");
+if (getAttribute!=null) {
+    switch (getAttribute) {
+
+';
+
+
+// example file location in $.data then use $json_data->data
+// set case where the data then set val for return data
+foreach ($json_data->data as $key => $value) {
+
+echo '
+    case "'.$value->branch_code.'":
+    val = "'.$value->branch_id.'";
+    return val;
+    break;
+';
+
+}
+
+
+echo '
+    }
+}
+return val;';
+
+
